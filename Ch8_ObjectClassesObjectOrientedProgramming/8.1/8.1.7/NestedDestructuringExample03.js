aaa4d30d02f775ccf572df9c55626f0399894d26
hikaricp <dockerc@163.com>
2023-03-25 22:19:49 +0800

    嵌套解构03-错误示例2

--- Ch8_ObjectClassesObjectOrientedProgramming/8.1/8.1.7/NestedDestructuringExample03.js
+++ Ch8_ObjectClassesObjectOrientedProgramming/8.1/8.1.7/NestedDestructuringExample03.js
@@ -1,3 +1,4 @@
+// 在外层属性没有定义的情况下不能使用解构
 let person = {
     job: {
         title: 'Software engineer'
@@ -6,9 +7,19 @@ let person = {
 
 let personCopy = {};
 
+// foo 在 person 中是 undefined 不能解构
 ({
     foo: {
         bar: personCopy.bar
+    }
+} = person);
+
+console.log(personCopy);
+
+// job 在 personCopy 中是 undefined
+({
+    job: {
+        title: personCopy.job.title
     }
 } = person);
 
