76a841dde2345e49b8a8a8ef20156b5f9889cc9b
hikaricp <dockerc@163.com>
2023-04-11 20:49:14 +0800

    没有重载

---
+++ Ch10_Functions/NoOverloadingExample02.js
@@ -0,0 +1,10 @@
+let addSomeNumber = function (num) {
+    return num + 100;
+};
+
+addSomeNumber = function (num) {
+    return num + 200;
+};
+
+let result = addSomeNumber(100); // 300
+console.log(result);
