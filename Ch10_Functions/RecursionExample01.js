21919b0fa5dd8770710174128d6e977a3c33ae74
hikaricp <dockerc@163.com>
2023-04-15 06:30:44 +0800

    递归

---
+++ Ch10_Functions/RecursionExample01.js
@@ -0,0 +1,11 @@
+function factorial(num) {
+    if (num <= 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+let anotherFactorial = factorial;
+factorial = null;
+console.log(anotherFactorial(4)); // 报错
---
+++ Ch10_Functions/RecursionExample02.js
@@ -0,0 +1,11 @@
+function factorial(num) {
+    if (num <= 1) {
+        return 1;
+    } else {
+        return num * arguments.callee(num - 1);
+    }
+}
+
+let anotherFactorial = factorial;
+factorial = null;
+console.log(anotherFactorial(4));
