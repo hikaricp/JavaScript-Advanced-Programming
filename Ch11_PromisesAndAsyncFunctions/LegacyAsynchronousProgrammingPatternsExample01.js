852f02dc1bc18b7189ab520dbc4384841224f7e3
hikaricp <dockerc@163.com>
2023-04-24 22:43:34 +0800

    格式化LegacyAsynchronousProgrammingPatternsExample01.js

--- Ch11_PromisesAndAsyncFunctions/LegacyAsynchronousProgrammingPatternsExample01.js
+++ Ch11_PromisesAndAsyncFunctions/LegacyAsynchronousProgrammingPatternsExample01.js
@@ -1,5 +1,5 @@
 function double(value) {
-    setTimeout(() => setTimeout(console.log, 0, value * 2), 1000);
+  setTimeout(() => setTimeout(console.log, 0, value * 2), 1000);
 }
 
 double(3); // 6 (大约 1000 毫秒之后)
